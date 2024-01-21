11209a789167396f9246712a3b5fab36337febe6
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2024-01-21 00:34:49 -0600

    Get objects in and tiles changing

--- index.js
+++ index.js
@@ -1,12 +1,75 @@
-const gameBoard = (function gameBoard() {
+const gameBoardSquares = document.querySelectorAll('.square');
+const gameBoardArray = Array.from(gameBoardSquares);
+
+const gameBoard = (function () {
     const gameGrid = [];
-    for(let i = 0; i < 9; i++){
-        gameGrid.push("");
+    let gameTurn = 1;
+    gameBoardSquares.forEach((square) => {
+        square = gameSquare();
+        gameGrid.push(square);
+    })
+    const turnTaken = () => gameTurn++;
+    const getNumTurns = () => gameTurn;
+
+    const checkForWinner = () => {
+        if (gameBoardSquares[0].getTextContent() === 'X' && gameBoardSquares[1].getTextContent() === 'X' && gameBoardSquares[2].getTextContent() === 'X') {
+            console.log('Player 1 is winner!');
+        } else if(gameBoardSquares[3].getTextContent() === 'X' && gameBoardSquares[4].getTextContent() === 'X' && gameBoardSquares[5].getTextContent() === 'X'){
+            console.log('Player 1 is winner!');
+        } else if(gameBoardSquares[6].getTextContent() === 'X' && gameBoardSquares[7].getTextContent() === 'X' && gameBoardSquares[8].getTextContent() === 'X'){
+        console.log('Player 1 is winner!');
+        }
     }
 
-    return {gameGrid}
+    return { gameGrid, turnTaken, getNumTurns, checkForWinner }
 })()
 
-function gameSquare () {
-    
+gameBoardArray.forEach((square) => {
+    Object.assign(square, gameSquare());
+    gameBoard.gameGrid.push(square);
+    square.addEventListener('click', () => {
+
+        if (!square.getIsMarked()) {
+            square.markSquare();
+            square.textContent = square.getTextContent();
+            gameBoard.checkForWinner();
+        }
+    })
+})
+
+const player1 = (function () {
+    const takeTurn = () => gameBoard.turnTaken();
+    return { takeTurn };
+})()
+
+const player2 = (function () {
+    const takeTurn = () => gameBoard.turnTaken();
+    return { takeTurn };
+})()
+
+function gameSquare() {
+    let isMarked = false;
+    let squareTextContent = '';
+    const getIsMarked = () => isMarked;
+    const markSquare = () => {
+        if (!isMarked) {
+            console.log('wtf');
+            console.log(player1.isMyTurn);
+            console.log(player2.isMyTurn);
+            if (gameBoard.getNumTurns() % 2 === 1) {
+                squareTextContent = 'X';
+                isMarked = true;
+                player1.takeTurn();
+            } else {
+                squareTextContent = 'O';
+                isMarked = true;
+                player2.takeTurn();
+            }
+            console.log(gameBoard.getNumTurns());
+        }
+    };
+    const getTextContent = () => squareTextContent;
+    return { isMarked, getIsMarked, markSquare, getTextContent };
 }
+
+

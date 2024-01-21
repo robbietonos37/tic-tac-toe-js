fed015adb431e224968a3e939f84349bb8ff5726
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2024-01-20 22:17:55 -0600

    Add gameboard object

---
+++ index.js
@@ -0,0 +1,12 @@
+const gameBoard = (function gameBoard() {
+    const gameGrid = [];
+    for(let i = 0; i < 9; i++){
+        gameGrid.push("");
+    }
+
+    return {gameGrid}
+})()
+
+function gameSquare () {
+    
+}
